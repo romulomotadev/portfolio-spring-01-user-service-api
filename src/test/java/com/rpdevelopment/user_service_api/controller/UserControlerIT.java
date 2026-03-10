9e25412b497688350cd6f0ecbc0b232a9c66e205
Romulo P T Mota <romuloptmota@gmail.com>
2026-03-10 13:41:21 -0300

    test: add integration tests for update user endpoint

--- src/test/java/com/rpdevelopment/user_service_api/controller/UserControlerIT.java
+++ src/test/java/com/rpdevelopment/user_service_api/controller/UserControlerIT.java
@@ -2,13 +2,11 @@ package com.rpdevelopment.user_service_api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rpdevelopment.user_service_api.dto.UserPersonAddressDto;
-import com.rpdevelopment.user_service_api.exception.ResourceNotFoundException;
 import com.rpdevelopment.user_service_api.repository.UserRepository;
 import com.rpdevelopment.user_service_api.service.UserPersonAddressService;
 import com.rpdevelopment.user_service_api.tests.UserFactoryDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,8 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -179,7 +176,7 @@ public class UserControlerIT {
                         .param("size", "1")
                         .accept(MediaType.APPLICATION_JSON));
 
-        //VALIDAÇÃO
+        //Validações
         resultActions.andExpect(status().isOk())
                 .andExpect(jsonPath("$.content").isNotEmpty())
                 .andExpect(jsonPath("$.content").isArray())
@@ -187,4 +184,105 @@ public class UserControlerIT {
                 .andExpect(jsonPath("$.number").value(0))
                 .andExpect(jsonPath("$.size").value(1));
     }
+
+    //================== UPDATE ==================
+
+    //ID EXISTENTE
+    @Test
+    public void updateShouldReturn200WhenIdExists() throws Exception {
+
+        //Preparando
+        dto.setName("Usuario Atualizado");
+        service.update(dto, existingId);
+
+        //Corpo da requisição
+        String requestBody = objectMapper.writeValueAsString(dto);
+
+        //Chamada / Ação
+        ResultActions resultActions =
+                mockMvc.perform(put("/users/{id}", existingId)
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        //Validações
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(existingId))
+                .andExpect(jsonPath("$.name").value("Usuario Atualizado"))
+                .andExpect(jsonPath("$.email").value("usuario@gmail.com"))
+                .andExpect(jsonPath("$.person.document").value("507.332.198-64"))
+                .andExpect(jsonPath("$.addresses[0].road").value("Rua dos Bobos"));
+    }
+
+    //ID NÃO EXISTENTE
+    @Test
+    public void updateShouldReturn404WhenIdDoesNotExist() throws Exception {
+
+        //Corpo da requisição
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        //Chamada / Ação
+        ResultActions resultActions =
+                mockMvc.perform(put("/users/{id}", nonExistingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        //Validações
+        resultActions.andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.error").value("Id Not Found"))
+                .andExpect(jsonPath("$.path").value("/users/" + nonExistingId));
+    }
+
+    //EMAIL DUPLICADO
+    @Test
+    public void updateShouldReturn409WhenEmailExists() throws Exception {
+
+        //Preparando
+        dto.setEmail("email@gmail.com");
+        service.save(dto);
+        dto.getPerson().setDocument("xxx.xxx.xxx-xx");
+
+        //Corpo da requisição
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        //PUT
+        ResultActions resultActions = mockMvc.perform(put("/users/{id}", existingId)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //RESPOSTA ESPERADA
+        resultActions.andExpect(status().isConflict())
+                .andExpect(jsonPath("$.status").value(409))
+                .andExpect(jsonPath("$.error").value("Email already exists"))
+                .andExpect(jsonPath("$.path").value("/users/" + existingId));
+    }
+
+    //DOCUMENTO DUPLICADO
+    @Test
+    public void updateShouldReturn409WhenDocumentExists() throws Exception {
+
+        //Preparando
+        dto.getPerson().setDocument("xxx.xxx.xxx-xx");
+        service.save(dto);
+        dto.setEmail("email@gmail.com");
+
+        //Corpo da requisição
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        //PUT
+        ResultActions resultActions = mockMvc.perform(put("/users/{id}", existingId)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //RESPOSTA ESPERADA
+        resultActions.andExpect(status().isConflict())
+                .andExpect(jsonPath("$.status").value(409))
+                .andExpect(jsonPath("$.error").value("Document already exists"))
+                .andExpect(jsonPath("$.path").value("/users/" + existingId));
+    }
+
 }
