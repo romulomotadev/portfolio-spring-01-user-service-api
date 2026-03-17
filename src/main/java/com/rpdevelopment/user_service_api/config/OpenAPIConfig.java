6c21922749a7795bbc66aff58bbb410299f87ac6
Romulo P T Mota <romuloptmota@gmail.com>
2026-03-17 14:26:33 -0300

    docs(swagger): apply global security to all endpoints with OpenApiCustomizer

--- src/main/java/com/rpdevelopment/user_service_api/config/OpenAPIConfig.java
+++ src/main/java/com/rpdevelopment/user_service_api/config/OpenAPIConfig.java
@@ -1,18 +1,29 @@
 package com.rpdevelopment.user_service_api.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer"
+)
 @OpenAPIDefinition
 @Configuration
 public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI userviewAPI() {
+    public OpenAPI userViewAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Users Service API")
@@ -22,5 +33,15 @@ public class OpenAPIConfig {
                                 .name("Apache 4.0")
                                 .url("https://github.com/romulomotadev/portfolio-spring-01-user-service-api")));
     }
+
+
+    @Bean
+    public OpenApiCustomizer globalSecurityOpenApiCustomizer() { // Mudou para Z
+        return openApi -> openApi.getPaths().values()
+                .forEach(pathItem -> pathItem.readOperations()
+                        .forEach(op -> op.addSecurityItem(new SecurityRequirement().addList("bearerAuth")))
+                );
+    }
+
 
 }
